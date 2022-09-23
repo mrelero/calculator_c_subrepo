b6d5e80a45b00731de5ea8adfaa1a65b97946eda
Matheus Raphael Elero <matheus.elero@setis>
2022-09-23 09:47:38 -0300

    git subrepo pull math_operations_h
    subrepo:
      subdir:   "math_operations_h"
      merged:   "4fbe647"
    upstream:
      origin:   "https://github.com/mrelero/math_operations_h"
      branch:   "main"
      commit:   "4fbe647"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

--- math_operations_h/operations.c
+++ math_operations_h/operations.c
@@ -4,3 +4,19 @@
 OPR_iSoma(int iNum1, int iNum2){
    return iNum1 + iNum2;
 }
+
+
+
+int OPR_iProduto(int iNum1, int iNum2){
+   return iNum1 * iNum2;
+}
+
+int OPR_iPotencia(int iNum1, int iExp){
+   int iAux;
+   int iMult;
+   iAux = 1;
+   for(iMult = 1; iMult <= iExp; iMult++){
+      iAux = iNum1 * iAux;
+   }
+   return iAux;
+}
--- math_operations_h/operations.h
+++ math_operations_h/operations.h
@@ -6,6 +6,9 @@
 
 extern int OPR_iSoma(int iNum1, int iNum2);
 
+extern int OPR_iProduto(int iNum1, int iNum2);
+
+extern int OPR_iPotencia(int iNum1, int iExp);
 
 
 #endif /* _CRT_SEC_INCLUDED_ */
