dd9cb0f7d7f0c1c2fd47ef6509910063f2d9c31b
Matheus Raphael Elero <matheus.elero@setis>
2022-09-23 09:22:30 -0300

    git subrepo clone https://github.com/mrelero/math_operations_h
    subrepo:
      subdir:   "math_operations_h"
      merged:   "16dbbab"
    upstream:
      origin:   "https://github.com/mrelero/math_operations_h"
      branch:   "main"
      commit:   "16dbbab"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

---
+++ math_operations_h/operations.c
@@ -0,0 +1,6 @@
+#include "operations.h"
+
+
+OPR_iSoma(int iNum1, int iNum2){
+   return iNum1 + iNum2;
+}
---
+++ math_operations_h/operations.h
@@ -0,0 +1,15 @@
+#ifndef _OPERATIONS_INCLUDED_
+#define _OPERATIONS_INCLUDED_
+
+
+#include <stdint.h>
+
+extern int OPR_iSoma(int iNum1, int iNum2);
+
+
+
+#endif /* _CRT_SEC_INCLUDED_ */
+
+
+
+
